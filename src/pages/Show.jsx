892b3169d9f512435962a58b30fd5c1b7462ec37
Half mind <avisaini909@gmail.com>
2023-07-22 14:53:33 +0530

    Read More button brings user to new tab now, added go back to home link on the show page

--- src/pages/Show.jsx
+++ src/pages/Show.jsx
@@ -1,5 +1,5 @@
 import { useQuery } from '@tanstack/react-query';
-import { useParams } from 'react-router-dom';
+import { Link, useParams } from 'react-router-dom';
 import { getShowById } from '../api/tvmaze';
 import ShowMainData from '../components/shows/ShowMainData';
 import Details from '../components/shows/Details';
@@ -20,8 +20,11 @@ const Show = () => {
     }
 
     if (showData){
-      return (<div>
-        
+      return (
+      <div>
+
+        <Link to="/">Go back to Home.</Link>
+
         <ShowMainData 
         image = {showData.image} 
         name={showData.name} 
