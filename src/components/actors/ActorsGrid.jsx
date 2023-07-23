f77c14f8d062dc16f59d95606f01de79b9853bf6
Half mind <avisaini909@gmail.com>
2023-07-24 00:30:29 +0530

    styled the entire application using styled components

--- src/components/actors/ActorsGrid.jsx
+++ src/components/actors/ActorsGrid.jsx
@@ -1,8 +1,9 @@
+import { FlexGrid } from "../common/FlexGrid";
 import ActorCard from "./ActorCard";
 
 const ActorsGrid = ({ actors }) => {
     return (
-        <div>
+        <FlexGrid>
         {actors.map(data => (
         <ActorCard key= {data.person.id} 
         name={data.person.name} 
@@ -13,7 +14,7 @@ const ActorsGrid = ({ actors }) => {
         image = {data.person.image ? data.person.image.medium : '/not-found-image.png'} 
         />
         ))}
-        </div>
+        </FlexGrid>
     );
 };
 
