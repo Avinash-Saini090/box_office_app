e3b12c757400731f94b9e31af60dafe1d28c5c69
Half mind <avisaini909@gmail.com>
2023-07-24 18:09:28 +0530

    added fade-in animation to FlexGrid. Added scale animation to StarBtn

--- src/components/shows/ShowCard.jsx
+++ src/components/shows/ShowCard.jsx
@@ -1,10 +1,27 @@
 import { styled } from "styled-components";
 import { SearchCard, SearchImgWrapper } from "../common/SearchCard";
 import { StarIcon } from "../common/StarIcon";
+import { useRef } from "react";
 
 const ShowCard = ({ name, image, id, summary, onStarMeClick, isStarred }) => {
 
-    const summaryStripped = summary ? summary.split(" ").slice(0,10).join(' ').replace(/<.+?>/g,'') : '... ';
+    const summaryStripped = summary ? summary.split(" ").slice(0,10).join(' ').replace(/<.+?>/g,'')+ '... ' : 'No description';
+
+    const starBtnRef = useRef();
+
+    const handleStarClick = () => {
+      onStarMeClick(id);
+
+      const starBtnEl = starBtnRef.current;
+
+      if (!starBtnEl) return;
+
+      if (isStarred) {
+        starBtnEl.classList.remove('animate');
+      } else {
+        starBtnEl.classList.add('animate');
+      }
+    };
     
     return (
     <SearchCard>
@@ -20,7 +37,7 @@ const ShowCard = ({ name, image, id, summary, onStarMeClick, isStarred }) => {
             <a href={`/show/${id}`} target="blank" rel="noreferrer">
                 Read more
             </a>
-            <StarBtn type="button" onClick={() => onStarMeClick(id) }>
+            <StarBtn ref={starBtnRef} type="button" onClick={handleStarClick}>
                 <StarIcon active={isStarred}/>
             </StarBtn>
         </ActionSection>
@@ -43,7 +60,7 @@ const ActionSection = styled.div`
       color: blue;
     }
   }
-`;
+`; 
 
 const StarBtn = styled.button`
   outline: none;
@@ -56,5 +73,21 @@ const StarBtn = styled.button`
   align-items: center;
   &:hover {
     cursor: pointer;
+  }
+  &.animate {
+    ${StarIcon} {
+      animation: increase 0.75s ease-in forwards;
+      @keyframes increase {
+        0% {
+          transform: scale(1);
+        }
+        50% {
+          transform: scale(3) rotate(45deg);
+        }
+        100% {
+          transform: scale(1);
+        }
+      }
+    }
   }
 `;
